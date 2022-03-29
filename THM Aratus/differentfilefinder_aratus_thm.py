3a6b51a5c01d71d17b74f652fb1b9a3f7c65606d
Praison001 <60835238+Praison001@users.noreply.github.com>
2022-03-29 20:25:54 +0530

    Update differentfilefinder_aratus_thm.py

--- THM Aratus/differentfilefinder_aratus_thm.py
+++ THM Aratus/differentfilefinder_aratus_thm.py
@@ -7,7 +7,7 @@ for subdir, dirs, files in os.walk(rootdir):
         textfile= os.path.join(subdir, file)
         
         with open(textfile,"rb") as f:
-            bytes = f.read() # read file as bytes
+            bytes = f.read()
             readable_hash = hashlib.md5(bytes).hexdigest();
             if readable_hash!= "3b485abefcd07db1f3aa83b25b41e56f":
                 print("Found a different hash!!!" + textfile + ":" + readable_hash + "<=== Better open this file and check what's up!")
